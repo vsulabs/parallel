075d214545452b8b6897ce8ff786b28bc710dec3
Diadlo <polsha3@gmail.com>
2018-10-20 00:32:53 +0300

    Fix

--- src/barber.cpp
+++ src/barber.cpp
@@ -10,9 +10,9 @@
 #include <unistd.h>
 #include "Semaphore.h"
 
-constexpr int CHAIRS_NUM = 25;
-constexpr int COME_TIME = 1;
-constexpr int BARBER_TIME = 1;
+constexpr int CHAIRS_NUM = 5;
+constexpr int COME_TIME = 3;
+constexpr int BARBER_TIME = 5;
 
 SemaphoreImpl barberSleep(1);
 SemaphoreImpl customersWait(CHAIRS_NUM);
@@ -32,91 +32,61 @@ void randSleep(int time)
     printSem.post(); \
 } while(0)
 
-#define C(x) DEBUG("CUST", #x)
-#define B(x) DEBUG("BURB", #x)
-
-void fillQueue(const std::string& filename, std::queue<int>& queue, std::atomic<size_t>& count)
+void fillQueue(const std::string& filename, std::queue<int>& queue)
 {
     std::ifstream f(filename);
     int val;
     while (f >> val) {
-        DEBUG("", "");
         randSleep(COME_TIME);
 
         accessSeats.wait();
         DEBUG("Cusomer come", val);
         if (queue.size() == CHAIRS_NUM) {
             // skip if no free chairs
+            DEBUG("Cusomer gone", val);
             accessSeats.post();
             continue;
         }
 
-        C(push(val));
+        DEBUG("Cusomer took a turn", val);
         queue.push(val);
         accessSeats.post();
 
-        C(barberSleep.post());
         barberSleep.post();
-        C(customersWait.wait());
-        customersWait.wait();
-
-        accessSeats.wait();
-        C(pop);
-        queue.pop();
-        if (queue.size() == 0)
-            count--;
-        accessSeats.post();
     }
-
-    C(finish);
 }
 
-void barber(std::queue<int>& queue, const std::atomic<size_t>& count)
+void barber(std::queue<int>& queue)
 {
-    while (queue.size() != 0 || count > 0) {
-        DEBUG("", "");
-        accessSeats.wait();
-        if (queue.size() == 0 && count == 0) {
-            accessSeats.post();
-            return;
-        }
-
-        accessSeats.post();
-        DEBUG("queue.size", queue.size());
-        DEBUG("count", count);
-
-        B(barberSleep.wait());
+    while (true) {
         barberSleep.wait();
 
         accessSeats.wait();
-        int val = queue.front();
+        const int val = queue.front();
+        queue.pop();
         accessSeats.post();
 
         DEBUG("Barber starts", val);
         randSleep(BARBER_TIME);
-
-        B(customersWait.post());
-        customersWait.post();
     }
 }
 
 int main(int argc, char *argv[])
 {
-    std::vector<std::string> files = { "1.txt", "2.txt" };
-    std::atomic<size_t> count{files.size()};
+    std::vector<std::string> files = { "1.txt", "2.txt", "3.txt" };
     std::queue<int> queue;
 
     barberSleep.wait();
     for (int i = 0; i < CHAIRS_NUM; ++i)
         customersWait.wait();
 
-    std::thread barberThread(barber, std::ref(queue), std::ref(count));
+    std::thread barberThread(barber, std::ref(queue));
 
     std::vector<std::thread> customersThread;
     for (auto& filename : files) {
         randSleep(COME_TIME);
         customersThread.push_back(std::thread(fillQueue, std::ref(filename),
-                    std::ref(queue), std::ref(count)));
+                    std::ref(queue)));
     }
 
     for (auto& thread : customersThread) {
